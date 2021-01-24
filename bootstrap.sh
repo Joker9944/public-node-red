0d1eba619a93e2db0e6466571c09d2d563321634
Joker9944 <9194199+Joker9944@users.noreply.github.com>
2021-01-24 23:19:19 +0100

    Fix path

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,7 @@ chmod 600 secrets/acme.json
 htpasswd -bc secrets/htpasswd admin "$PW"
 chmod 600 secrets/htpasswd
 
-mkdir -p etc/node-red-data
+mkdir -p etc/node-red/data
 sudo chown -R 1000:1000 etc/node-red/data
 
 cp .env.example .env
