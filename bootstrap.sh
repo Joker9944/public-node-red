ffa1077f33bb101819ee60230eb0b41e2fcb86fd
Joker9944 <9194199+Joker9944@users.noreply.github.com>
2021-01-24 23:08:48 +0100

    Inital commit

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+PW=$1
+
+mkdir  secrets
+chmod 700 secrets
+touch secrets/acme.json
+chmod 600 secrets/acme.json
+
+htpasswd -bc secrets/htpasswd admin "$PW"
+chmod 600 secrets/htpasswd
+
+mkdir -p etc/node-red-data
+sudo chown -R 1000:1000 etc/node-red/data
+
+cp .env.example .env
+
+docker-compose pull
